afac3b7705dedef4ec780a43b2fcc3353072851d
ricardo-nascimento-cloud <ricardonascimento1506@gmail.com>
2026-02-03 19:40:39 -0300

    ajustes

--- php/authenticate.php
+++ php/authenticate.php
@@ -1,8 +1,8 @@
-<?php
-session_start();
+    <?php
+    session_start();
 
-if (!isset($_SESSION['user_id'])) {
-    header('Location: user-login.php');
-    exit();
-}
-?>
+    if (!isset($_SESSION['user_id'])) {
+        header('Location: user-login.php');
+        exit();
+    }
+    ?>
